eaadb3ba2be64a78626e6bb187b4eba5fc25fdeb
AMANI <kalaiara@gmail.com>
2025-06-11 18:57:22 -0500

    Boolean,Primitive & Non Primitive Literals

--- src/ex_02_Java_Basic/Lab027_Task_Mulitplicationtable.java
+++ src/ex_02_Java_Basic/Lab027_Task_Mulitplicationtable.java
@@ -6,36 +6,33 @@ public class Lab027_Task_Mulitplicationtable {
         int b = 1;
 
         System.out.println("Multiplication table");
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*2);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*3);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*4);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*5);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*6);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*7);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*8);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
+        System.out.printf("%d x %d = %d",a,b,a*9);
         b = b + 1;
         System.out.println();
-        System.out.printf("%d x %d",a,b);
-
-
-
+        System.out.printf("%d x %d = %d",a,b,a*10);
     }
 }
