150e098d93df9db3362d315894d18ab82921eba3
AMANI <kalaiara@gmail.com>
2025-06-11 18:58:16 -0500

    Boolean,Primitive & Non Primitive Literals

---
+++ src/ex_03_Literals/Lab031_BooleanLiterals.java
@@ -0,0 +1,7 @@
+package ex_03_Literals;
+
+public class Lab031_BooleanLiterals {
+    boolean First = true;
+    boolean Second = false;
+//         boolean is_married_amit = 123; not valid
+}
---
+++ src/ex_03_Literals/Lab032_Enum.java
@@ -0,0 +1,8 @@
+package ex_03_Literals;
+
+public class Lab032_Enum {
+    public static void main(String[] args) {
+        int Enum = 6; // enum is keyword
+        System.out.println(Enum);
+    }
+}
