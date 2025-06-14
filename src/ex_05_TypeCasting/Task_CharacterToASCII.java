210cc0a0787b4f2734bb947468f4f0d786f4b38e
AMANI <kalaiara@gmail.com>
2025-06-14 17:05:17 -0500

    TypeCast Homework

---
+++ src/ex_05_TypeCasting/Task_CharacterToASCII.java
@@ -0,0 +1,10 @@
+package ex_05_TypeCasting;
+
+public class Task_CharacterToASCII {
+    public static void main(String[] args) {
+        //Description: Typecasts a character to its ASCII integer value. Example Print A ASCII Int value.
+        char c = 'A';
+        int b = (int)c;
+        System.out.println("ASCII Value of A is: " +b);
+    }
+}
---
+++ src/ex_05_TypeCasting/Task_TypeCasting_InDivision.java
@@ -0,0 +1,11 @@
+package ex_05_TypeCasting;
+
+public class Task_TypeCasting_InDivision {
+    public static void main(String[] args) {
+        //Description: int a = 10, int b = 3; now divide 10/3 and store the decimal value in the Result variable and print it.
+        int a = 10;
+        int b = 3;
+        float result = (float) a/b;
+        System.out.println(result);
+    }
+}
