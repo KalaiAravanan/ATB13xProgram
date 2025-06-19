0c21b868b1c0b51905d0fd155cb43ab7724299a2
AMANI <kalaiara@gmail.com>
2025-06-18 21:27:47 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ src/Test/Challenge2.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+       // Use +, -, *, /, % operators
+         //       - Demonstrate assignment operators (=, +=, -=, *=, /=)
+        //- Show the results of each operation
+
+        int a = 10;
+        int b=5;
+        System.out.println("Addition: " +(a+b));
+        System.out.println("Substraction: "+ (a-b));
+        System.out.println("Mulitplication: " + (a*b));
+        System.out.println("Division: "+ (a/b));
+        int c =a%b;
+        System.out.println("Modulus: " +c);
+
+
+    }
+}
