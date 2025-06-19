58e9c8727750f867c8eb071267c448c6a8277b8a
AMANI <kalaiara@gmail.com>
2025-06-18 21:21:13 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ src/Test/Challenge1.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        // Declare and initialize variables of different primitive data types (int, double, boolean, char)
+        //Print the values of all variables
+        //Show proper naming conventions
+        int i=1;
+        double j = 2.3;
+        boolean k = true;
+        char l = 'A';
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+    }
+}
