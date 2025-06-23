444f384ffdb9560a438b36b4b0afa86ccdd2593e
AMANI <kalaiara@gmail.com>
2025-06-23 18:33:54 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_13_Functions/Lab_129_MethodFunctions.java
@@ -0,0 +1,25 @@
+package ex_13_Functions;
+
+public class Lab_129_MethodFunctions {
+       public static void main(String[] args) {
+            //Step 2 - Function Call
+            f1();
+            f2();
+            //name_of_function();
+        }
+        static void f2(){
+            System.out.println("This is printed from function f2");
+        }
+        // Step 1- Function Def.
+        static void f1(){
+            System.out.println("Hi");
+        }
+
+        static void name_of_function(){
+            System.out.println("THIS IS THE CODE THAT YOU WANT TO EXECUTE");
+            System.out.println("THIS IS THE CODE THAT YOU WANT TO EXECUTE");
+            System.out.println("THIS IS THE CODE THAT YOU WANT TO EXECUTE");
+            System.out.println("THIS IS THE CODE THAT YOU WANT TO EXECUTE");
+        }
+    }
+
